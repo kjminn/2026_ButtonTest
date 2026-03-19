02743878c13a801fbb9bbd9e2e3078666f009a07
kjminn <kjminn2001@gmail.com>
2026-03-19 16:24:30 +0900

    View 속성, 리스너, 기본위젯의 다양한 속성들

--- app/src/main/java/kr/ac/kopo/buttontest/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/buttontest/MainActivity.java
@@ -1,6 +1,12 @@
 package kr.ac.kopo.buttontest;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,32 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit1;
+    TextView text1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_main2);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        Button btn1 = findViewById(R.id.btn1);
+        edit1 = findViewById(R.id.edit1);
+        text1 = findViewById(R.id.text1);
+        btn1.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String name = edit1.getText().toString();
+            Toast.makeText(getApplicationContext(), name + "님 환영합니다.", Toast.LENGTH_LONG).show();
+            text1.setText(name + "님 입장~~");
+            text1.setTextColor(Color.MAGENTA);
+        }
+    };
 }
